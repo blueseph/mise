398b3d36a0093fe89ed843c17d7726423b02b8c1
dan blanco <blueseph@gmail.com>
2019-06-23 14:40:48 -0700

    add husky, lint-staged, prettier

--- src/queue/fiber.js
+++ src/queue/fiber.js
@@ -1,25 +1,25 @@
-import { createElement } from '../vdom';
+import { createElement } from '../vdom/dom';
 
 export const types = {
   create: 'CREATE',
   remove: 'REMOVE',
   replace: 'REPLACE',
-  update: 'UPDATE',
+  update: 'UPDATE'
 };
 
 export const create = ({
   parent,
   element = null,
   previous = null,
-  next = null,
+  next = null
 }) => ({
   parent,
   previous: {
     tree: previous,
-    element,
+    element
   },
   next: {
     tree: next,
-    element: next !== null ? createElement(next) : next,
-  },
+    element: next !== null ? createElement(next) : next
+  }
 });
